87ce157cdf848e306f996655481e80568eb0f4e2
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2022-09-12 20:32:48 -0300

    chore: add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { theme, GlobalStyles } from 'styles';
+import { ThemeProvider } from 'styled-components';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
